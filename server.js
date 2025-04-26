3c037d9382c39d0af303aa6533be15c2400e38de
kaksv <kakoozavian@gmail.com>
2025-04-26 20:11:22 +0300

    removed stats

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const statsRouter = require('./routes/statsRoute');
+// const statsRouter = require('./routes/statsRoute');
 const cors = require('cors');
 
 dotenv.config();
@@ -17,7 +17,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/hackathons', require('./routes/hackathonRoutes')); // Add hackathon routes
-app.use('/api/stats', statsRouter); // Add project routes
+// app.use('/api/stats', statsRouter); // Add project routes
 
 // Connect to DB and start server
 connectDB().then(() => {
